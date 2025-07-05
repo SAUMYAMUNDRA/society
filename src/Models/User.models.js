87d4754bb88741a32a2385f5f50c6acf99bed5e7
SAUMYA MUNDRA <mundrasaumya17@gmail.com>
2025-07-05 11:35:06 +0530

    RNOVATION

--- src/Models/User.models.js
+++ src/Models/User.models.js
@@ -1,19 +1,21 @@
 import mongoose, { Schema } from "mongoose";
+
 const UserSchema = new mongoose.Schema({
-  name: { type: String  },
-  email: { type: String},
+  name: { type: String, required: true },
+  age: { type: Number },
+  email: { type: String, required: true, unique: true },
   phone: { type: String, required: true },
-//   dob: { type: Date, required: true },
-//   societyName: { type: String, required: true },
-//   societyAddress: { type: String, required: true },
-  password: { type: String},
-//   cpass: { type: String, required: true },
+  dob: { type: Date }, // Optional: use if needed
+  address: { type: String }, // Optional
+  password: { type: String, required: true },
   flatNo: { type: String, required: true },
-  secretaryId:{
-    type:Schema.Types.ObjectId,
-    ref:"Secretary",
-    require:true
+
+  secretaryId: {
+    type: Schema.Types.ObjectId,
+    ref: "Secretary",
+    required: true
   },
+
   role: {
     type: String,
     enum: ["member", "worker"],
